d50d1c7cf6c0c076ece9211a9c25aa9989a9eb4f
SivanYad <justsivan@gmail.com>
2020-10-24 00:30:37 +0300

    I did the chuck app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      joke: ''
+    }
+    this.getJoke = this.getJoke.bind(this);
+  }
+  async getJoke() {
+    try {
+      const response = await axios.get('https://api.chucknorris.io/jokes/random');
+      this.setState({ joke: response.data.value })
+      
+    } catch(error) {
+      console.log(error);
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Chuck Norris</h1>
+        <button onClick={this.getJoke}>Get Chuck Norris</button>
+        <h2>{this.state.joke}</h2>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/ChuckButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ChuckButton extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ChuckButton;
